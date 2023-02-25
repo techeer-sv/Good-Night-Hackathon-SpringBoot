93923eab9338a5543a5219f01417572fa38be9ba
hangyeol <snake011128@naver.com>
2023-02-26 03:24:36 +0900

    feat: Entity 추가

---
+++ src/main/java/com/techeer/hackathon/domain/restaurant/entity/Restaurant.java
@@ -0,0 +1,32 @@
+package com.techeer.hackathon.domain.restaurant.entity;
+
+import com.techeer.hackathon.global.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="restaurant")
+public class Restaurant extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Builder
+    public Restaurant(String category, String name) {
+        this.category = category;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/techeer/hackathon/domain/review/entity/Review.java
@@ -0,0 +1,37 @@
+package com.techeer.hackathon.domain.review.entity;
+
+import com.techeer.hackathon.domain.restaurant.entity.Restaurant;
+import com.techeer.hackathon.global.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="review")
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant", nullable = false)
+    private Restaurant restaurant;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Review(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
