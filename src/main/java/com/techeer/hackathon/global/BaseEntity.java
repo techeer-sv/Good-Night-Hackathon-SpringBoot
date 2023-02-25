32bab7b8d89d336b4fce138c778a9bcdf407df6f
zyoon <develop_z@naver.com>
2023-02-26 04:30:46 +0900

    fix: baseentity

--- src/main/java/com/techeer/hackathon/global/BaseEntity.java
+++ src/main/java/com/techeer/hackathon/global/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.techeer.hackathon.global;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)  // Auditing(자동으로 값 매핑)
+@Getter
 public class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
