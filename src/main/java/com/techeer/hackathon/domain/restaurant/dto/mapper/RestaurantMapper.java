e8977e9100181a9dd690c2a67f30e9b5298a6a5d
hangyeol <snake011128@naver.com>
2023-02-26 05:13:37 +0900

    feat: Mapper 추가

---
+++ src/main/java/com/techeer/hackathon/domain/restaurant/dto/mapper/RestaurantMapper.java
@@ -0,0 +1,28 @@
+package com.techeer.hackathon.domain.restaurant.dto.mapper;
+
+import com.techeer.hackathon.domain.restaurant.dto.RegisterRestaurantDTO;
+import com.techeer.hackathon.domain.restaurant.dto.InquiryRestaurantDTO;
+import com.techeer.hackathon.domain.restaurant.entity.Restaurant;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Component
+public class RestaurantMapper {
+    public Restaurant DtoToEntity(RegisterRestaurantDTO registerRestaurantDTO) {
+        return Restaurant.builder()
+                .name(registerRestaurantDTO.getName())
+                .category(registerRestaurantDTO.getCategory())
+                .build();
+    }
+
+    public InquiryRestaurantDTO DtoFromEntity(Restaurant restaurant) {
+        return InquiryRestaurantDTO.builder()
+                .id(restaurant.getId())
+                .name(restaurant.getName())
+                .category(restaurant.getCategory())
+                .build();
+    }
+
+}
