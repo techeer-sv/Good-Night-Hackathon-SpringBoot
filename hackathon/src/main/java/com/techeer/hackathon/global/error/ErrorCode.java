880849c5b591cfcb1104053e5e08342370d68449
JeongHunHui <deca3248@gmail.com>
2023-02-26 04:07:31 +0900

    feat: 에러코드를 관리하는 Enum 추가

---
+++ hackathon/src/main/java/com/techeer/hackathon/global/error/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.techeer.hackathon.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // Global
+    INTERNAL_SERVER_ERROR(500, "G001", "서버 오류"),
+    INPUT_INVALID_VALUE(409, "G002", "잘못된 입력"),
+
+    // Restaurant
+    RESTAURANT_DUPLICATION_ERROR(409, "R001", "레스토랑 이름이 중복됩니다.");
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
+
