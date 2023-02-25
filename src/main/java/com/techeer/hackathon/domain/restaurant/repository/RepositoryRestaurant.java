aab6751a0f74e51ca8db4b50fc93f123febd380a
hangyeol <snake011128@naver.com>
2023-02-26 05:14:31 +0900

    feat: 레포지토리 추가

--- src/main/java/com/techeer/hackathon/domain/restaurant/repository/RepositoryRestaurant.java
+++ src/main/java/com/techeer/hackathon/domain/restaurant/repository/RepositoryRestaurant.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface repositoryRestaurant extends JpaRepository<Restaurant, Long> {
+public interface RepositoryRestaurant extends JpaRepository<Restaurant, Long> {
 
 }
