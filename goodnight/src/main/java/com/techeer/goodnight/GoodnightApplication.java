1b2759c145d42ed02bf41559a828b4f8d606e87f
Ray Lee <wooo519@naver.com>
2023-02-26 00:00:06 +0900

    first settings

---
+++ goodnight/src/main/java/com/techeer/goodnight/GoodnightApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.goodnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodnightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodnightApplication.class, args);
+	}
+
+}
---
+++ goodnight/src/test/java/com/techeer/goodnight/GoodnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeer.goodnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodnightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
