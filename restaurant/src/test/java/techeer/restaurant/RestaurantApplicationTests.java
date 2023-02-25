ad496e37383ae5715da6eea5a25f04be3d75e0af
Ellie <doris0707@naver.com>
2023-02-25 23:27:12 +0900

    chore: initial settings

---
+++ restaurant/src/test/java/techeer/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package techeer.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
