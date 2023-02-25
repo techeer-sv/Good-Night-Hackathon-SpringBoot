28622ae408d28d93f76e02dabb0ceaeed2ecc6e4
hangyeol <snake011128@naver.com>
2023-02-26 05:40:55 +0900

    feat: 레스토랑 추가 API

--- src/main/java/com/techeer/hackathon/domain/restaurant/controller/ControllerRestaurant.java
+++ src/main/java/com/techeer/hackathon/domain/restaurant/controller/ControllerRestaurant.java
@@ -1,9 +1,35 @@
 package com.techeer.hackathon.domain.restaurant.controller;
 
+import com.techeer.hackathon.domain.restaurant.dto.InquiryRestaurantDTO;
+import com.techeer.hackathon.domain.restaurant.dto.RegisterRestaurantDTO;
+import com.techeer.hackathon.domain.restaurant.dto.mapper.RestaurantMapper;
+import com.techeer.hackathon.domain.restaurant.entity.Restaurant;
+import com.techeer.hackathon.domain.restaurant.service.ServiceRestaurant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/api/v1/restaurant")
+@RestController
+@RequiredArgsConstructor
 public class ControllerRestaurant {
+    private final ServiceRestaurant Res_Service;
+    private final RestaurantMapper Res_Mapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<InquiryRestaurantDTO> createRestaurant(
+            @RequestBody @Valid RegisterRestaurantDTO registerRestaurantDTO) {
+        Restaurant insertRestaurant = Res_Service.insertRestaurant(registerRestaurantDTO);
+        return new ResponseEntity(Res_Mapper.DtoFromEntity(insertRestaurant), HttpStatus.CREATED);
+    }
 }
---
+++ src/main/java/com/techeer/hackathon/domain/restaurant/service/ServiceRestaurant.java
@@ -0,0 +1,19 @@
+package com.techeer.hackathon.domain.restaurant.service;
+
+import com.techeer.hackathon.domain.restaurant.dto.RegisterRestaurantDTO;
+import com.techeer.hackathon.domain.restaurant.dto.mapper.RestaurantMapper;
+import com.techeer.hackathon.domain.restaurant.entity.Restaurant;
+import com.techeer.hackathon.domain.restaurant.repository.RepositoryRestaurant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ServiceRestaurant {
+    private final RepositoryRestaurant Res_Repo;
+    private final RestaurantMapper Res_Mapper;
+
+    public Restaurant insertRestaurant(RegisterRestaurantDTO registerRestaurantDTO) {
+        return Res_Repo.save(Res_Mapper.DtoToEntity(registerRestaurantDTO));
+    }
+}
