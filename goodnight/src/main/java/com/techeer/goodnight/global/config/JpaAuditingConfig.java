f7941661b015f88edf4eb16dcb81b00074e036cb
Ray Lee <wooo519@naver.com>
2023-02-26 07:59:25 +0900

    make config

---
+++ goodnight/src/main/java/com/techeer/goodnight/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.techeer.goodnight.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
