b345cf3dcd17ec9675b6ba497d07f1b6174d08db
조성현 <agllwy116@naver.com>
2023-02-26 06:19:53 +0900

    fix : 카테고리 enum 수정 및 entity 수정

--- Hackathon/src/main/java/com/example/hackathon/restaurant/domain/entity/Category.java
+++ Hackathon/src/main/java/com/example/hackathon/restaurant/domain/entity/Category.java
@@ -1,5 +1,14 @@
 package com.example.hackathon.restaurant.domain.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Category {
-    한식,중식,일식
+    KOREAN_RESTAURANT("한식"),
+    CHINESE_RESTAURANT("중식"),
+    JAPANESE_RESTAURANT("일식");
+
+    private final String category;
 }
--- Hackathon/src/main/java/com/example/hackathon/restaurant/domain/entity/Restaurant.java
+++ Hackathon/src/main/java/com/example/hackathon/restaurant/domain/entity/Restaurant.java
@@ -2,8 +2,12 @@ package com.example.hackathon.restaurant.domain.entity;
 
 import com.example.hackathon.global.entity.BaseTimeEntity;
 import com.example.hackathon.review.domain.entity.Review;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.util.List;
@@ -11,23 +15,32 @@ import java.util.List;
 @Entity
 @Getter
 @Table(name = "restaurant")
-@NoArgsConstructor
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Restaurant extends BaseTimeEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "restaurant_id" , nullable = false)
     private Long id;
 
-    @Column(name = "name" , nullable = false)
+    @Column(nullable = false, length = 30)
     private String name;
 
     @Column(name = "category", nullable = false)
     @Enumerated(EnumType.STRING)
     private Category category;
 
+    // Review 와 1:n 관계
     @OneToMany(mappedBy = "restaurant")
     private List<Review> reviews;
 
-    public void deleteRestaurant() { this.activeOff(); }
+    @Builder
+    public Restaurant(String name, Category category) {
+        this.name = name;
+        this.category = category;
+        this.isActive = true;
+    }
+
+    public void deleteRestaurant() { this.activeOff();}
 }
