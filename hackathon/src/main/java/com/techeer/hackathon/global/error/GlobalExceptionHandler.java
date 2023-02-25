e2b79ff32f175d283230a547f1ce4dd6209c4390
JeongHunHui <deca3248@gmail.com>
2023-02-26 04:11:00 +0900

    feat: GlobalExceptionHandler 추가

---
+++ hackathon/src/main/java/com/techeer/hackathon/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.techeer.hackathon.global.error;
+
+import com.techeer.hackathon.global.error.exceptions.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.techeer.hackathon.global.error.ErrorCode.INPUT_INVALID_VALUE;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleRuntimeException(BusinessException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse response =
+                ErrorResponse.builder()
+                        .errorMessage(errorCode.getMessage())
+                        .businessCode(errorCode.getCode())
+                        .build();
+        log.warn(e.getMessage());
+        return ResponseEntity.status(errorCode.getStatus()).body(response);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(INPUT_INVALID_VALUE, e.getBindingResult());
+        log.warn(e.getMessage());
+        return ResponseEntity.status(INPUT_INVALID_VALUE.getStatus()).body(response);
+    }
+}
