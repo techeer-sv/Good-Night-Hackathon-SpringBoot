31d214a1dcd462cde138283634a9b8ded0e217d0
zyoon <develop_z@naver.com>
2023-02-25 23:50:24 +0900

    setting : spring boot 프로젝트

---
+++ src/test/java/com/example/techeer/TecheerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
