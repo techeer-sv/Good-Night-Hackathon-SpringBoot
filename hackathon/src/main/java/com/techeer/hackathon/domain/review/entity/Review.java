24404f94cd00891aee633734a71398cf481ba28d
JeongHunHui <deca3248@gmail.com>
2023-02-26 02:53:29 +0900

    feat: 리뷰 엔티티 생성

---
+++ hackathon/src/main/java/com/techeer/hackathon/domain/review/entity/Review.java
@@ -0,0 +1,46 @@
+package com.techeer.hackathon.domain.review.entity;
+
+import com.techeer.hackathon.domain.restaurant.entity.Restaurant;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "REVIEW")
+@EntityListeners(AuditingEntityListener.class)
+public class Review {
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Review(
+            Restaurant restaurant,
+            String title,
+            String content
+    ) {
+        this.restaurant = restaurant;
+        this.title = title;
+        this.content = content;
+    }
+}
