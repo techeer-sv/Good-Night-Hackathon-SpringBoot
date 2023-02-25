9ee09e9fdd29467c8ec3a8fd8e1a1db1ace7e531
hangyeol <snake011128@naver.com>
2023-02-26 00:51:14 +0900

    프로젝트 build

---
+++ src/main/java/com/techeer/hackathon/TecheerApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheerApplication.class, args);
+	}
+
+}
