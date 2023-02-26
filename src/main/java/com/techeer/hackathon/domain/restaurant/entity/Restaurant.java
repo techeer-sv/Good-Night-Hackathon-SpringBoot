0d72cc1ef3d5747df7fe7579455f9ee81ed0e862
hangyeol <snake011128@naver.com>
2023-02-26 17:22:54 +0900

    chore: 파일 수정

--- src/main/java/com/techeer/hackathon/domain/restaurant/entity/Restaurant.java
+++ src/main/java/com/techeer/hackathon/domain/restaurant/entity/Restaurant.java
@@ -1,7 +1,10 @@
 package com.techeer.hackathon.domain.restaurant.entity;
 
 import com.techeer.hackathon.global.BaseEntity;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
