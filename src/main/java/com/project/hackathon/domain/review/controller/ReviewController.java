e5a75f5eb84ae9db5280bc8d61e0b3933d251e49
HAERYN <syt06119@naver.com>
2023-02-26 15:27:33 +0900

    feat: review patch 추가

--- src/main/java/com/project/hackathon/domain/review/controller/ReviewController.java
+++ src/main/java/com/project/hackathon/domain/review/controller/ReviewController.java
@@ -3,17 +3,22 @@ package com.project.hackathon.domain.review.controller;
 
 import com.project.hackathon.domain.restaurant.dto.RestaurantCreateRequest;
 import com.project.hackathon.domain.restaurant.dto.RestaurantDetailResponse;
+import com.project.hackathon.domain.restaurant.dto.RestaurantUpdateRequest;
+import com.project.hackathon.domain.restaurant.entity.Category;
 import com.project.hackathon.domain.restaurant.entity.Restaurant;
 import com.project.hackathon.domain.restaurant.service.RestaurantService;
 import com.project.hackathon.domain.review.dto.ReviewCreateRequest;
 import com.project.hackathon.domain.review.dto.ReviewDetailResponse;
+import com.project.hackathon.domain.review.dto.ReviewUpdateRequest;
 import com.project.hackathon.domain.review.entity.Review;
 import com.project.hackathon.domain.review.service.ReviewService;
+import com.project.hackathon.global.dto.ResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/api/v1/{restaurant}/review")
@@ -35,4 +40,27 @@ public class ReviewController {
         List<Review> reviews = reviewService.getAllReviews();
         return ResponseEntity.ok(reviews);
     }
+
+//    @GetMapping("/list/{category}")
+//    public ResponseEntity<List<RestaurantDetailResponse>> loadRestaurantByCategory(@PathVariable Category category) {
+//        List<RestaurantDetailResponse> restaurantDetailResponses = restaurantService.getRestaurantsByCategory(category);
+//        return ResponseEntity.ok(restaurantDetailResponses);
+//    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ReviewDetailResponse> updateReview(
+            @PathVariable Long id,
+            @Valid @RequestBody ReviewUpdateRequest request) {
+        ReviewDetailResponse reviewDetailResponse = reviewService.update(id, request);
+        return ResponseEntity.ok(reviewDetailResponse);
+    }
+
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<RestaurantDetailResponse> deleteRestaurant(
+//            @PathVariable Long id) {
+//        restaurantService.delete(id);
+//        String message = ResultResponse.RESTAURANT_DELETE_SUCCESS.getMessage();
+//        RestaurantDetailResponse response = new RestaurantDetailResponse(message);
+//        return ResponseEntity.ok(response);
+//    }
 }
