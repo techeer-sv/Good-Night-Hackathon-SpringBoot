a1b89306bda2b08b47752ec9160e08337c3e879a
fnzl54 <rnjscksdud1@naver.com>
2023-03-07 04:33:48 +0900

    feat: gradle 세팅

---
+++ Good-Night-Hackathon/src/main/java/chanyoung/GoodNightHackathon/GoodNightHackathonApplication.java
@@ -0,0 +1,13 @@
+package chanyoung.GoodNightHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodNightHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodNightHackathonApplication.class, args);
+	}
+
+}
---
+++ Good-Night-Hackathon/src/test/java/chanyoung/GoodNightHackathon/GoodNightHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package chanyoung.GoodNightHackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodNightHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
