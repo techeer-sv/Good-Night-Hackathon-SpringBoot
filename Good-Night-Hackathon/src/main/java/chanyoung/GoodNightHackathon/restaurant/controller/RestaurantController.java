a54176accbaca341d055ed65208e85a6daa71cbe
fnzl54 <rnjscksdud1@naver.com>
2023-03-14 02:04:58 +0900

    refactor: response status 추가

--- Good-Night-Hackathon/src/main/java/chanyoung/GoodNightHackathon/restaurant/controller/RestaurantController.java
+++ Good-Night-Hackathon/src/main/java/chanyoung/GoodNightHackathon/restaurant/controller/RestaurantController.java
@@ -5,6 +5,7 @@ import chanyoung.GoodNightHackathon.restaurant.dto.requeset.RestaurantRequest;
 import chanyoung.GoodNightHackathon.restaurant.dto.requeset.RestaurantRequestId;
 import chanyoung.GoodNightHackathon.restaurant.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,32 +18,38 @@ public class RestaurantController {
     private final RestaurantService service;
 
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public void saveRestaurant (@RequestBody RestaurantRequest request) {
         service.save(request);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Restaurant> findTotalRestaurant () {
         return service.findTotal();
     }
 
     @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void updateRestaurant(@RequestBody RestaurantRequestId request) {
         service.update(request.getId(), request.getCategory());
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Optional<Restaurant> findIdRestaurant (@PathVariable Long id) {
         return service.findId(id);
     }
 
     @GetMapping("/categorys")
+    @ResponseStatus(HttpStatus.OK)
     public List<Restaurant> findCategoryRestaurant (@RequestParam(value = "category") String category) {
         System.out.println(category);
         return service.findCategory(category);
     }
 
     @GetMapping("/remove/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeRestaurant (@PathVariable Long id) {
         service.removeRestaurant(id);
     }
--- Good-Night-Hackathon/src/main/java/chanyoung/GoodNightHackathon/review/controller/ReviewController.java
+++ Good-Night-Hackathon/src/main/java/chanyoung/GoodNightHackathon/review/controller/ReviewController.java
@@ -9,6 +9,7 @@ import chanyoung.GoodNightHackathon.review.dto.ReviewResponse;
 import chanyoung.GoodNightHackathon.review.service.ReviewService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,26 +23,31 @@ public class ReviewController {
 
     private final ReviewService service;
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public void saveReview (@RequestBody ReviewRequest request) {
         service.save(request);
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ReviewResponse findIdReview (@PathVariable Long id) {
         return service.findId(id);
     }
 
     @PutMapping("/updates")
+    @ResponseStatus(HttpStatus.CREATED)
     public void updateReview(@RequestBody ReviewRequestId request) {
         service.updateReview(request.getId(), request.getTitle(), request.getContent());
     }
 
     @GetMapping("/removes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeReview (@PathVariable Long id) {
         service.removeReview(id);
     }
 
     @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
     public List<ReviewResponse> findByTitleContent (@RequestBody ReviewRequestOrder request) {
         return service.sortReview(request.getTitle(), request.getContent(), request.getOrderType());
     }
